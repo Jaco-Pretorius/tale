1e412002f41716b65fc41195257bb8f4d5b28d2f
Jaco Pretorius <jaco.pretorius@betterment.com>
2017-12-28 17:55:31 -0600

    Adding s3_website config and release script

---
+++ release.sh
@@ -0,0 +1,34 @@
+#!/bin/bash --login
+
+set -a
+
+ENVIRONMENT_NAME=$1
+
+if [ -z "$ENVIRONMENT_NAME" ]; then
+  echo "No environment specified!"
+  echo "Usage: ./release.sh environment"
+  exit 1
+fi
+
+echo "Loading environment variables for $ENVIRONMENT_NAME"
+echo "Sourcing .$ENVIRONMENT_NAME.config"
+echo
+
+source ".$ENVIRONMENT_NAME.config"
+
+if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ] || [ -z "$AWS_S3_BUCKET" ]; then
+  echo "Not all expected variables are present, stopping deployment"
+  exit 1
+fi
+
+echo "Building website"
+export JEKYLL_ENV=$ENVIRONMENT_NAME
+bundle exec jekyll build
+echo
+
+echo "Deploying to bucket: $AWS_S3_BUCKET"
+bundle exec s3_website push
+
+if [ -n "$WEBSITE_URL" ]; then
+  open $WEBSITE_URL
+fi
